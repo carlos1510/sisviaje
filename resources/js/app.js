743d10b07af4cf9b9040e4fc368d88b7cc226b1e
carlos1510 <carloscle1510@gmail.com>
2022-09-30 21:34:50 -0500

    Subiendo el proyecto sisviaje con laravel 8 y vue 3

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+import { createApp } from 'vue'
+
+import Home from './components/Home.vue';
+
+const app = createApp({});
+
+app.component('home', Home);
+
+app.mount('#app');
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Welcome to the VUE 3</h1>
+        <p>Installing vue 3 in laravel from scrath has been donde successfull</p>
+    </div>
+</template>
